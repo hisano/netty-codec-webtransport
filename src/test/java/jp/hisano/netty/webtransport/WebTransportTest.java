e589d898de4e3eae021b34f25787150b5f1452eb
Koji Hisano <hisano@gmail.com>
2024-07-02 08:06:32 +0900

    Add `Playwright 1.44.0` for test

--- src/test/java/jp/hisano/netty/webtransport/WebTransportTest.java
+++ src/test/java/jp/hisano/netty/webtransport/WebTransportTest.java
@@ -1,9 +1,18 @@
 package jp.hisano.netty.webtransport;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.Test;
 
 public class WebTransportTest {
 	@Test
 	public void testBasic() {
+		try (Playwright playwright = Playwright.create()) {
+			Browser browser = playwright.chromium().launch();
+			Page page = browser.newPage();
+			page.navigate("http://playwright.dev");
+			System.out.println(page.title());
+		}
 	}
 }
