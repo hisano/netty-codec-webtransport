ea214eace0d333916cddf1fd193972bfe4ad932b
Koji Hisano <hisano@gmail.com>
2024-07-12 02:23:04 +0900

    Add SETTINGS_* constants

--- src/main/java/jp/hisano/netty/webtransport/WebTransportSessionHandler.java
+++ src/main/java/jp/hisano/netty/webtransport/WebTransportSessionHandler.java
@@ -10,12 +10,17 @@ import io.netty.incubator.codec.quic.QuicStreamChannel;
 import io.netty.util.ReferenceCountUtil;
 
 public final class WebTransportSessionHandler extends ChannelInboundHandlerAdapter {
+	private static final long SETTINGS_ENABLE_CONNECT_PROTOCOL = 0x08L;
+	private static final long SETTINGS_H3_DATAGRAM = 0x33L;
+	private static final long SETTINGS_ENABLE_WEBTRANSPORT = 0x2b60_3742L;
+	private static final long SETTINGS_WEBTRANSPORT_MAX_SESSIONS = 0xc671_706aL;
+
 	public static Http3SettingsFrame createSettingsFrameFromServer() {
 		DefaultHttp3SettingsFrame settingsFrame = new DefaultHttp3SettingsFrame();
-		settingsFrame.put(0x08L, 1L);
-		settingsFrame.put(0x33L, 1L);
-		settingsFrame.put(0x2b60_3742L, 1L);
-		settingsFrame.put(0xc671_706aL, 256L);
+		settingsFrame.put(SETTINGS_ENABLE_CONNECT_PROTOCOL, 1L);
+		settingsFrame.put(SETTINGS_H3_DATAGRAM, 1L);
+		settingsFrame.put(SETTINGS_ENABLE_WEBTRANSPORT, 1L);
+		settingsFrame.put(SETTINGS_WEBTRANSPORT_MAX_SESSIONS, 256L);
 		return settingsFrame;
 	}
 
@@ -35,8 +40,8 @@ public final class WebTransportSessionHandler extends ChannelInboundHandlerAdapt
 		System.out.println("WebTransport stream created: streamId = " + ((QuicStreamChannel) ctx.channel()).streamId());
 
 		Http3HeadersFrame responseFrame = new DefaultHttp3HeadersFrame();
-		responseFrame.headers().secWebtransportHttp3Draft("draft02");
 		responseFrame.headers().status("200");
+		responseFrame.headers().secWebtransportHttp3Draft("draft02");
 		ctx.writeAndFlush(responseFrame);
 
 		ReferenceCountUtil.release(msg);
