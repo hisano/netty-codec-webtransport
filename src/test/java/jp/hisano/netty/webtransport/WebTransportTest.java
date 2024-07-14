99d86926a097953e4576de70cd344eb3b88eaf52
Koji Hisano <hisano@gmail.com>
2024-07-14 13:28:53 +0900

    Change testcase timeout

--- src/test/java/jp/hisano/netty/webtransport/WebTransportTest.java
+++ src/test/java/jp/hisano/netty/webtransport/WebTransportTest.java
@@ -103,7 +103,7 @@ public class WebTransportTest {
 			});
 			page.navigate("https://localhost:4433/");
 			page.textContent("*").contains("LOADED");
-			waiter.await(30, TimeUnit.SECONDS);
+			waiter.await(10, TimeUnit.SECONDS);
 		}
 	}
 
